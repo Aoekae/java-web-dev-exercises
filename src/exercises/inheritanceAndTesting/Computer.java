717f76b99d5b20b4b516b30cabf3675844ee1151
Aoekae <dark_aoekae@yahoo.com>
2022-09-06 14:23:40 -0500

    Created files, created fields and constructor, getters and setters for Computer.java

---
+++ src/exercises/inheritanceAndTesting/Computer.java
@@ -0,0 +1,47 @@
+package exercises.inheritanceAndTesting;
+
+public class Computer {
+    private final String model;
+    private final Processor processor;
+    private User user;
+    private String operatingSystem;
+    private int hardDriveCapacity;
+
+    public Computer(String model, Processor processor, User user, String operatingSystem, int hardDriveCapacity) {
+        this.model = model;
+        this.processor = processor;
+        this.user = user;
+        this.operatingSystem = operatingSystem;
+        this.hardDriveCapacity = hardDriveCapacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Processor getProcessor() {
+        return processor;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public int getHardDriveCapacity() {
+        return hardDriveCapacity;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+
+}
---
+++ src/exercises/inheritanceAndTesting/Processor.java
@@ -0,0 +1,4 @@
+package exercises.inheritanceAndTesting;
+
+public class Processor {
+}
